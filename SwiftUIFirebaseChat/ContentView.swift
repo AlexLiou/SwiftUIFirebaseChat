4f0778efd7e2c43c5f08122c380318f8ff9d8030
AlexLiou <alexliou0@gmail.com>
2022-03-14 00:56:16 -0400

    Initial Commit

---
+++ SwiftUIFirebaseChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by Alex Liou on 3/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by Alex Liou on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
